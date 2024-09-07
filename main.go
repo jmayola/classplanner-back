4966d1408fa2735004aad05f730fff4b24c38c11
mayola <mayola@marketpsychdata.com>
2024-09-06 23:39:26 -0300

    functional gin & sql-driver

--- main.go
+++ main.go
@@ -1,74 +1,64 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
-var db = make(map[string]string)
+type User struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+//var db = make(map[string]string)
 
 func setupRouter() *gin.Engine {
-	// Disable Console Color
-	// gin.DisableConsoleColor()
 	r := gin.Default()
 
-	// Ping test
 	r.GET("/ping", func(c *gin.Context) {
 		c.String(http.StatusOK, "pong")
 	})
 
-	// Get user value
-	r.GET("/user/:name", func(c *gin.Context) {
-		user := c.Params.ByName("name")
-		value, ok := db[user]
-		if ok {
-			c.JSON(http.StatusOK, gin.H{"user": user, "value": value})
-		} else {
-			c.JSON(http.StatusOK, gin.H{"user": user, "status": "no value"})
+	r.POST("/login", func(c *gin.Context) {
+		var newUser User
+		if err := c.BindJSON(&newUser); err != nil {
+			return
 		}
-	})
-
-	// Authorized group (uses gin.BasicAuth() middleware)
-	// Same than:
-	// authorized := r.Group("/")
-	// authorized.Use(gin.BasicAuth(gin.Credentials{
-	//	  "foo":  "bar",
-	//	  "manu": "123",
-	//}))
-	authorized := r.Group("/", gin.BasicAuth(gin.Accounts{
-		"foo":  "bar", // user:foo password:bar
-		"manu": "123", // user:manu password:123
-	}))
-
-	/* example curl for /admin with basicauth header
-	   Zm9vOmJhcg== is base64("foo:bar")
-
-		curl -X POST \
-	  	http://localhost:8080/admin \
-	  	-H 'authorization: Basic Zm9vOmJhcg==' \
-	  	-H 'content-type: application/json' \
-	  	-d '{"value":"bar"}'
-	*/
-	authorized.POST("admin", func(c *gin.Context) {
-		user := c.MustGet(gin.AuthUserKey).(string)
-
-		// Parse JSON
-		var json struct {
-			Value string `json:"value" binding:"required"`
-		}
-
-		if c.Bind(&json) == nil {
-			db[user] = json.Value
-			c.JSON(http.StatusOK, gin.H{"status": "ok"})
-		}
+		//data := c.BindJSON(&newUser)
+		c.IndentedJSON(http.StatusAccepted, newUser)
 	})
 
 	return r
 }
 
 func main() {
+	db, err := sql.Open("mysql", "root@tcp(localhost:3306)/reports")
+	if err != nil {
+		panic(err.Error()) // Just for example purpose. You should use proper error handling instead of panic
+	}
+	rows, err := db.Query("SELECT username FROM users")
+	if err != nil {
+		panic(err.Error()) // Just for example purpose. You should use proper error handling instead of panic
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var name string
+
+		// Escanea los datos de la fila en variables
+		if err := rows.Scan(&name); err != nil {
+			panic(err.Error()) // Just for example purpose. You should use proper error handling instead of panic
+		}
+		// Imprime los valores de la fila
+		fmt.Printf("Name: %s\n", name)
+	}
+	defer rows.Close()
 	r := setupRouter()
 	// Listen and Server in 0.0.0.0:8080
-	r.Run(":8080")
+	r.Run(":3000")
 }
