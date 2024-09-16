41727ed354cc37012de2361425a1b5569d329c98
mayola <mayola@marketpsychdata.com>
2024-09-16 15:18:06 -0300

    solved cors

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -12,53 +11,100 @@ import (
 )
 
 type User struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
+	Name     string `json:"user_name"`
+	Password string `json:"user_password"`
+	LastName string `json:"user_lastname"`
+	Mail     string `json:"user_mail"`
+	Type     string `json:"user_type"`
+	Alias    string `json:"user_alias"`
 }
 
-//var db = make(map[string]string)
 
 func setupRouter() *gin.Engine {
+	//declaring database connection
+	db, err := sql.Open("mysql", "root@tcp(localhost:3306)/classplanner")
+	if err != nil {
+		panic(err.Error())
+	}
+
 	r := gin.Default()
 
 	r.GET("/ping", func(c *gin.Context) {
 		c.String(http.StatusOK, "pong")
 	})
 
+	// login
+	// login
+	// login
+
 	r.POST("/login", func(c *gin.Context) {
 		var newUser User
+		//getting data sended
 		if err := c.BindJSON(&newUser); err != nil {
 			return
 		}
-		//data := c.BindJSON(&newUser)
-		c.IndentedJSON(http.StatusAccepted, newUser)
+		//preparing statement
+		users, err := db.Prepare("SELECT user_name FROM users WHERE user_mail=? AND user_password=?")
+		if err != nil {
+			c.Status(505)
+		}
+		defer users.Close()
+		//setting query output
+		var outp string
+		err = users.QueryRow(newUser.Mail, newUser.Password).Scan(&outp)
+		if err != nil {
+			c.String(http.StatusForbidden, "Los datos ingresados no son correctos")
+		} else {
+			c.String(http.StatusAccepted, "Ingreso Exitoso"+outp)
+		}
 	})
 
+	// registerrr
+	// registerrr
+	// registerrr
+
+	r.POST("/register", func(c *gin.Context) {
+		var newUser User
+		//getting data sended
+		if err := c.BindJSON(&newUser); err != nil {
+			return
+		}
+		//preparing statement
+		users, err := db.Prepare("INSERT INTO `users` (`id_user`, `user_name`, `user_lastname`, `user_password`, `user_mail`, `user_type`, `user_alias`) VALUES (NULL, ?, ?, ?, ?, ?, ?);")
+		if err != nil {
+			c.Status(505)
+		}
+		defer users.Close()
+		//setting query output
+
+		_, err = users.Exec(newUser.Name, newUser.LastName, newUser.Password, newUser.Mail, newUser.Type, "")
+		if err != nil {
+			c.Status(403)
+		} else {
+			c.String(http.StatusAccepted, "Ingreso Exitoso ")
+		}
+	})
 	return r
 }
+func CORSMiddleware() gin.HandlerFunc {
+    return func(c *gin.Context) {
 
+        c.Header("Access-Control-Allow-Origin", "*")
+        c.Header("Access-Control-Allow-Credentials", "true")
+        c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+        c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT")
+
+        if c.Request.Method == "OPTIONS" {
+            c.AbortWithStatus(204)
+            return
+        }
+
+        c.Next()
+    }
+}
 func main() {
-	db, err := sql.Open("mysql", "root@tcp(localhost:3306)/reports")
-	if err != nil {
-		panic(err.Error()) // Just for example purpose. You should use proper error handling instead of panic
-	}
-	rows, err := db.Query("SELECT username FROM users")
-	if err != nil {
-		panic(err.Error()) // Just for example purpose. You should use proper error handling instead of panic
-	}
-	defer rows.Close()
-	for rows.Next() {
-		var name string
-
-		// Escanea los datos de la fila en variables
-		if err := rows.Scan(&name); err != nil {
-			panic(err.Error()) // Just for example purpose. You should use proper error handling instead of panic
-		}
-		// Imprime los valores de la fila
-		fmt.Printf("Name: %s\n", name)
-	}
-	defer rows.Close()
 	r := setupRouter()
 	// Listen and Server in 0.0.0.0:8080
+	r.Use(CORSMiddleware())	
 	r.Run(":3000")
 }
